145ca19df92fba0711d9fbfad0d690a29c41a18d
Sergio Daniel Tamietto <sergiotamietto@hotmail.com>
2022-11-06 01:13:09 -0300

    inicio

--- proyecto/inicio.py
+++ proyecto/inicio.py
@@ -1,3 +1,16 @@
-import formPrincipal as form1
-def main():
-    form1
+import pandas as pd
+import os
+import csv
+
+dirActual=os.getcwd()
+
+with open(dirActual + "/proyecto/precios_en_surtidor.csv","r") as arch:
+    lectura = csv.reader(arch, delimiter=",")
+    
+    contador =0 
+    for rows in lectura:
+        print(rows.count)
+        print(rows[1])
+        contador+=1
+        if contador>3:
+            break
